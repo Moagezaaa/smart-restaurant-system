712d659902a0ea710c08a4249480360580e1eeb4
Mohammed Nasser <mohammednassermoageza@gmail.com>
2025-10-19 06:30:39 +0300

    add verifyToken of orders

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const orderController = require("../controllers/orders");
+const verifyToken = require("../middlewares/verifyToken");
 router.post("/createOrder", orderController.createOrder);
 router.delete("/deleteOrder", orderController.deleteOrder);
-router.get("/gettableOrders", orderController.gettableOrders);
+router.get("/gettableOrders", verifyToken,orderController.gettableOrders);
 module.exports = router;
